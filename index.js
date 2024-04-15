8a277046f315c6133f489cc19efd040e605f4c33
Joseph-Chege <mjchege23@gmail.com>
2024-04-15 14:59:13 +0300

    Created a function to add an event listener

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+
+    //Assign input variable to button element
+    const input = document.getElementById('button');
+
+    //Callback function that is called and returns an alert on click
+    function clickAlert() {
+        alert('I was clicked!');   
+    }
+    input.addEventListener('click', clickAlert);
 }
+
